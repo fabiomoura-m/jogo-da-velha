aea4f212b034c88fe7bfd55fa05c912cb778d904
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-04-10 21:04:29 -0300

    feat: inclui função que verifica o vencedor

--- assets/js/script.js
+++ assets/js/script.js
@@ -78,4 +78,26 @@ function checkGame() {
   }
 }
 
-function checkWinnerFor(player) {}
+function checkWinnerFor(player) {
+  let possibilities = [
+    'a1,a2,a3',
+    'b1,b2,b3',
+    'c1,c2,c3',
+
+    'a1,b1,c1',
+    'a2,b2,c2',
+    'a3,b3,c3',
+
+    'a1,b2,c3',
+    'c1,b2,a3'
+  ];
+
+  for (let pItem in possibilities) {
+    let pArray = possibilities[pItem].split(','); // a1, a2, a3
+    let hasWon = pArray.every(option => square[option] === player);
+    if (hasWon) {
+      return true;
+    }
+  }
+  return false;
+}
